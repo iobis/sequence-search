6fbf315a2c26278be1e2f1d11bb4dd0eee0af441
Pieter Provoost <pieterprovoost@gmail.com>
2023-01-13 00:12:23 +0100

    removed fastq input

--- generate_input.py
+++ generate_input.py
@@ -9,19 +9,9 @@ TTAAAGTTGTTGTGGTTAAAAAGCTCGTAGTTGGATCTCAACAGACCTGGATCGGTTTACTTTGTGTAATACTGATCCAG
 """.replace("\n", "")
 
 try:
-    os.remove("input.fastq")
     os.remove("input.fasta")
 except FileNotFoundError:
     pass
-
-with open("input.fastq", "w") as fastq_file:
-    record = SeqRecord(
-        Seq(seq),
-        id="x",
-        description="input sequence"
-    )
-    record.letter_annotations["phred_quality"] = [40] * len(record.seq)
-    SeqIO.write([record], fastq_file, "fastq")
 
 with open("input.fasta", "w") as fasta_file:
     record = SeqRecord(
